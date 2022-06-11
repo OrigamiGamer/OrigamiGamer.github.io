226e718e0a6efa4310e2cf492c1d1880aaee3cfa
OrigamiGamer <3084943362@qq.com>
2022-06-11 21:27:23 +0800

    123

---
+++ js/basic.js
@@ -0,0 +1,11 @@
+{
+    let _face = "portrait.gitee.com/uploads/avatars/user/3450/10350908_OrigamiGame_1642737826.png!avatar200"
+    function initialize() {
+        // 加载头像
+        var img = document.getElementById("img-face");
+        img.setAttribute("src", _face);
+    }
+
+    initialize();
+
+}
