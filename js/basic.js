edcc06dc9d1148c57fe9ac96bcbb59caaa1e5c1b
OrigamiGamer <3084943362@qq.com>
2022-06-13 00:22:37 +0800

    1

--- js/basic.js
+++ js/basic.js
@@ -1,11 +1,18 @@
 {
-    let _face = "portrait.gitee.com/uploads/avatars/user/3450/10350908_OrigamiGame_1642737826.png!avatar200"
+    let _face = "https://portrait.gitee.com/uploads/avatars/user/3450/10350908_OrigamiGame_1642737826.png!avatar200"
     function initialize() {
         // 加载头像
-        var img = document.getElementById("img-face");
+        var img = document.getElementById("img_face");
         img.setAttribute("src", _face);
     }
 
-    initialize();
 
+    function friendship_blog(name) {
+        switch (name) {
+            case "creakler":
+                window.location.href = "https://creaklerblog.freeflarum.com";
+        }
+    }
+
+    initialize();
 }
