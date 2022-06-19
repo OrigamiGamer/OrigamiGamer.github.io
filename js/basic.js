ceb3b63fa4ea846bf48a7a2d38b8564ff6fbee63
OrigamiGamer <3084943362@qq.com>
2022-06-19 09:29:10 +0800

    new

--- js/basic.js
+++ js/basic.js
@@ -1,9 +1,11 @@
 {
-    let _face = "https://portrait.gitee.com/uploads/avatars/user/3450/10350908_OrigamiGame_1642737826.png!avatar200"
     function initialize() {
+        //资源
+        var src_face = "https://portrait.gitee.com/uploads/avatars/user/3450/10350908_OrigamiGame_1642737826.png!avatar200"
+
         // 加载头像
-        var img = document.getElementById("img_face");
-        img.setAttribute("src", _face);
+        //var img = document.getElementById("face");
+        //img.setAttribute("src",src_face);
 
         //加载图标
         //var ico = document.getElementsByClassName("blog-icon");
