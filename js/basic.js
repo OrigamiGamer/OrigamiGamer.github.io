55d684c3fe26cd993f87764a22fc757a8d8e3b26
OrigamiGamer <3084943362@qq.com>
2022-06-29 00:19:14 +0800

    new

--- js/basic.js
+++ js/basic.js
@@ -1,18 +1,16 @@
 {
-    function initialize() {
-        //资源
-        var src_face = "https://portrait.gitee.com/uploads/avatars/user/3450/10350908_OrigamiGame_1642737826.png!avatar200"
+    // 加载网站所有资源
+    function load_images() {
 
-        // 加载头像
-        //var img = document.getElementById("face");
-        //img.setAttribute("src",src_face);
-
-        //加载图标
-        //var ico = document.getElementsByClassName("blog-icon");
     }
 
+    // 加载网站文章
+    function load_articles() {
+        marked_parse("content", "First article.md");
+    }
 
-    function friendship_blog(name) {
+    // 跳转网页
+    function target(name) {
         var _blog;
         switch (name) {
             case "creakler":
@@ -25,4 +23,15 @@
         window.location.href = _blog;
     }
 
+    // 每天首次进入网站时欢迎
+    function window_load() {
+        var _date = new Date;
+        var _today = toString(_date.getDay());
+        if (localStorage.getItem("LastLoggedTime") != _today) {
+            localStorage.setItem("LastLoggedTime", _today);
+        } else {
+            //document.getElementById("welcome").remove();
+        }
+    }
+    window.onload = window_load;
 }
