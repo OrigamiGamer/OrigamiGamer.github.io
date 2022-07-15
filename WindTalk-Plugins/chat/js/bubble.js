21951390258e9dd67ee0c566c6271077be2a7f83
OrigamiGamer <3084943362@qq.com>
2022-07-15 17:03:47 +0800

    new

--- WindTalk-Plugins/chat/js/bubble.js
+++ WindTalk-Plugins/chat/js/bubble.js
@@ -2,9 +2,9 @@
     console.log("气泡作者: Origami");
     console.log("  *添加消息 msg_add(pos,name,contnet,face);");
     console.log("  *清空消息 msg_clear();");
-    console.log("  *跳转位置 page_goto(pos);    pos: top,bottom");
+    console.log("  *跳转位置 page(pos);    pos: top,bottom");
 
-    //消息数据结构体
+    // 结构体-消息数据
     var chat_msg = {
         "pos": "",
         "name": "",
@@ -13,22 +13,29 @@
         "face": ""
     };
 
-    var msgs = chat_msg;
+    var AryMsgs = chat_msg; // 数组-消息数据
 
 
     //添加消息
     function msg_add(pos, name, account, content, face) {
         //储存到消息数据结构体
-        var n = ++msgs.length;
-
+        var n = ++AryMsgs.length;
         var _msg = chat_msg;
-        _msg.pos = pos;
-        _msg.name = name;
-        _msg.account = account;
-        _msg.content = content;
-        _msg.face = face;
-
-        msgs[n] = _msg;
+        if (pos != undefined) {
+            _msg.pos = pos;
+        }
+        if (name != undefined) {
+            _msg.name = name;
+        }
+        if (account != undefined) {
+            _msg.account = account;
+        }
+        if (content != undefined) {
+            _msg.content = content;
+        }
+        if (face != undefined) {
+            _msg.face = face;
+        }
 
 
         //获取基础元素
@@ -39,25 +46,25 @@
         var div_box = document.createElement("div");
 
 
-        if (pos == "center") {	//提示消息//
+        if (_msg.pos == "center") {	//提示消息//
 
             //设置class属性为chat-notice
             div_box.setAttribute("class", "chat-notice");
             //创建提示内容 span
             var span_content = document.createElement("span");
             //传递 content参数 给 span节点
-            span_content.innerHTML = content;
+            span_content.innerHTML = _msg.content;
             //将 span_content节点 设为 div_box节点 的子节点
             div_box.appendChild(span_content);
 
 
         } else {				//用户消息
 
-            if (pos == "left") {	//左侧消息
+            if (_msg.pos == "left") {	//左侧消息
                 //设置class属性为chat-sender
                 div_box.setAttribute("class", "chat-sender");
             }
-            if (pos == "right") {	//右侧消息
+            if (_msg.pos == "right") {	//右侧消息
                 //设置class属性为chat-receiver
                 div_box.setAttribute("class", "chat-receiver");
             }
@@ -67,8 +74,9 @@
             var div_face = document.createElement("div");
             //创建头像图片 img
             var img_face = document.createElement("img");
-            //传递 face参数 给 img节点的src属性
-            img_face.setAttribute("src", face);
+            //传递 face参数 给 img节点的class,src属性
+            img_face.setAttribute("class", "img-face");
+            img_face.setAttribute("src", _msg.face);
             //将 img_face节点 设为 div_face节点 的子节点
             div_face.appendChild(img_face);
             //将 div_face节点 设为 div_box节点 的子节点
@@ -79,7 +87,7 @@
             //名称
             var div_name = document.createElement("div");
             //传递 name参数 给 div_name节点
-            div_name.innerHTML = name;
+            div_name.innerHTML = _msg.name;
             //将 div_name节点 设为 div_box节点 的子节点
             div_box.appendChild(div_name);
 
@@ -91,11 +99,11 @@
             var div_triangle = document.createElement("div");
 
             //是否位于左侧
-            if (pos == "left") {
+            if (_msg.pos == "left") {
                 //设置class属性为chat-left_triangle
                 div_triangle.setAttribute("class", "chat-left_triangle");
             }
-            if (pos == "right") {
+            if (_msg.pos == "right") {
                 //设置class属性为chat-right_triangle
                 div_triangle.setAttribute("class", "chat-right_triangle");
             };
@@ -106,7 +114,7 @@
             //创建内容 span
             var span_content = document.createElement("span");
             //传递 content参数 给 span_content节点
-            span_content.innerHTML = content;
+            span_content.innerHTML = _msg.content;
             //将 span_content节点 设为 div_bubble节点 的子节点
             div_bubble.appendChild(span_content);
 
@@ -117,7 +125,9 @@
         //添加 消息div节点 到末尾 tail节点
         body.insertBefore(div_box, tail);
 
-        page_goto("bottom");
+        AryMsgs[n] = _msg;
+
+        page("bottom"); //页面到底部
     }
 
 
@@ -125,7 +135,7 @@
     function msg_clear() {
 
         //删除消息结构体
-        msgs.length = 0;
+        AryMsgs.length = 0;
 
         //删除右侧消息
         var div_left = document.getElementsByClassName("chat-sender");
@@ -154,12 +164,14 @@
 
 
     //网页移动到指定位置
-    function page_goto(pos) {
+    function page(pos) {
         switch (pos) {
             case "top":
-            //跳转到顶部不会写
+                //跳转顶部不会写
+                break;
             case "bottom":
                 document.documentElement.scrollTop = document.body.scrollHeight;
+                break;
         }
     }
 
